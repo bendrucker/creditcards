d3c0cd985a2476f7c33549ba481396307557dbc4
Ben Drucker <bvdrucker@gmail.com>
2015-06-02 09:54:05 -0400

    Remove vars from index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
 
-var card = exports.card = require('./src/card')
-var cvc = exports.cvc = require('./src/cvc')
-var expiration = exports.expiration = require('./src/expiration')
+exports.card = require('./src/card')
+exports.cvc = require('./src/cvc')
+exports.expiration = require('./src/expiration')
