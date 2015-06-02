68555cbb7464f9fe9bd8db859857fd9a4fb65b93
Ben Drucker <bvdrucker@gmail.com>
2015-06-02 10:01:55 -0400

    Style cleanup for card

--- src/card.js
+++ src/card.js
@@ -2,37 +2,41 @@
 
 var camel = require('camel-case')
 var luhn = require('fast-luhn')
+var types = require('creditcards-types').types
 
-exports.types = require('creditcards-types').types
+exports.types = types
 
-exports.parse = function (number) {
+exports.parse = function parseCard (number) {
   if (typeof number !== 'string') return ''
   return number.replace(/[^\d]/g, '')
 }
 
-exports.format = function (number, separator) {
+exports.format = function formatCard (number, separator) {
   var type = getType(number, true)
   if (!type) return number
   return type.group(number).join(separator || ' ')
 }
 
-exports.type = function (number, eager) {
+exports.type = function cardType (number, eager) {
   var type = getType(number, eager)
   return type ? type.name : undefined
 }
 
 exports.luhn = luhn
 
-exports.isValid = function (number, type) {
-  if (!type) type = exports.type(number)
-  type = exports.types[camel(type)]
+exports.isValid = function isCardValid (number, type) {
+  if (type) {
+    type = types[camel(type)]
+  } else {
+    type = getType(number)
+  }
   if (!type) return false
   return (!type.luhn || luhn(number)) && type.test(number)
 }
 
 function getType (number, eager) {
-  for (var typeName in exports.types) {
-    var type = exports.types[typeName]
-    if (type.test(number, eager)) return exports.types[typeName]
+  for (var typeName in types) {
+    var type = types[typeName]
+    if (type.test(number, eager)) return types[typeName]
   }
 }
