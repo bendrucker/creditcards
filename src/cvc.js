04737a9c4a38b0ee464cf49b0dee96700f119b99
Jarek Rencz <jarek.rencz@functionite.com>
2015-11-10 14:45:55 +0100

    Allow CVC to be a number
    Fix #8

--- src/cvc.js
+++ src/cvc.js
@@ -4,8 +4,9 @@ var types = require('./types')
 var cvcRegex = /^\d{3,4}$/
 
 exports.isValid = function cvcIsValid (cvc, type) {
-  if (typeof cvc !== 'string') return false
-  if (!cvcRegex.test(cvc)) return false
+  if (typeof cvc !== 'string' && typeof cvc !== 'number') return false
+  var cvcStr = cvc + ''
+  if (!cvcRegex.test(cvcStr)) return false
   if (!type) return true
-  return types.get(type).cvcLength === cvc.length
+  return types.get(type).cvcLength === cvcStr.length
 }
--- test/cvc.js
+++ test/cvc.js
@@ -5,16 +5,22 @@ var cvc = require('../').cvc
 
 test('cvc', function (t) {
   t.ok(cvc.isValid('123'))
+  t.ok(cvc.isValid(123))
   t.ok(cvc.isValid('1234'))
+  t.ok(cvc.isValid(1234))
   t.notOk(cvc.isValid('12'))
+  t.notOk(cvc.isValid(12))
   t.notOk(cvc.isValid('12345'))
+  t.notOk(cvc.isValid(12345))
 
   t.ok(cvc.isValid('123', 'visa'))
+  t.ok(cvc.isValid(123, 'visa'))
   t.notOk(cvc.isValid('1234', 'visa'))
+  t.notOk(cvc.isValid(1234, 'visa'))
   t.notOk(cvc.isValid('123', 'American Express'))
+  t.notOk(cvc.isValid(123, 'American Express'))
   t.ok(cvc.isValid('1234', 'americanExpress'))
-
-  t.notOk(cvc.isValid(123))
+  t.ok(cvc.isValid(1234, 'americanExpress'))
 
   t.end()
 })
