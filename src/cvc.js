df2ec44aee1fa05df7b10e58bf4bd07e4cdb7f59
James Alexander <jalexander@leafsoftwaresolutions.com>
2016-02-09 11:34:13 -0500

    - Reformat js to be more friendly with the ASP.NET minifier

--- src/cvc.js
+++ src/cvc.js
@@ -3,9 +3,11 @@
 var types = require('./types')
 var cvcRegex = /^\d{3,4}$/
 
-exports.isValid = function cvcIsValid (cvc, type) {
+function cvcIsValid (cvc, type) {
   if (typeof cvc !== 'string') return false
   if (!cvcRegex.test(cvc)) return false
   if (!type) return true
   return types.get(type).cvcLength === cvc.length
 }
+
+exports.isValid = cvcIsValid
